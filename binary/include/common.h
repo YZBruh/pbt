21e7daac0f9f0af048718ffd8a469611f6ff5a61
YZBruh <yagizzengin73@gmail.com>
2024-03-04 23:17:13 +0300

    pbt: add global variables, macros

--- binary/include/common.h
+++ binary/include/common.h
@@ -31,12 +31,41 @@
  * limitations under the License.
  */
 
+/* funcs */
 void error(const char *err_msg);
 void check_psf();
 void check_root();
 void listpart();
 void backup(char *target_pt, char *pst);
 void flash();
+
+/* macros */
+#define ANSI_RED      "\033[31m"
+#define ANSI_YELLOW   "\033[33m"
+#define ANSI_GREEN    "\033[32m"
+#define ANSI_RESET    "\033[0m"
+
+#define PACK_VER      "1.5.0"
+#define PACK_VER_CODE "150"
+#define PACK_NAME     "Partition Backupper"
+#define PACK_LANG     "en"
+
+/* variables */
+extern char *argx_target_p;
+extern char *out;
+extern char *outdir;
+extern char *my_out;
+extern char *cust_cxt;
+extern char cust_cxt_path[150];
+extern char cust_cxt_ckpath[150];
+extern char cust_cxt_cklpath[150];
+extern char path[300];
+extern char cmd[256];
+extern bool use_argx_p;
+extern bool use_logical;
+extern bool use_cust_cxt;
+extern bool pbt_ab;
+extern bool pbt_logical;
 
 /* end of code */
 
