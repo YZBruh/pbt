c88c529d2caf611eab854c45c5ea511bc3199586
YZBruh <yagizzengin73@gmail.com>
2024-03-06 20:28:11 +0300

    pmt: initial 1.5.0 update

--- binary/include/documentation.h
+++ binary/include/documentation.h
@@ -1,22 +1,9 @@
-#ifndef _DOCUMENTATION_H_
 #define _DOCUMENTATION_H_
 
 /* By YZBruh */
 
 /*
- *         _      _   
- *        | |    | |  
- *  _ __  | |__  | |_ 
- * | '_ \ | '_ \ | __|
- * | |_) || |_) || |_ 
- * | .__/ |_.__/  \__|
- * | |                
- * |_|                
- *
- */
-
-/*
- * Copyright 2024 YZBruh - Partition Backupper
+ * Copyright 2024 YZBruh - Partition Manager
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +20,7 @@
 
 void licenses()
 {
-    printf("Copyright 2024 YZBruh - Partition Backupper\n");
+    printf("Copyright 2024 YZBruh - Partition Manager\n");
     printf("Licensed under the Apache License, Version 2.0 (the \"License\");\n");
     printf("you may not use this file except in compliance with the License.\n");
     printf("You may obtain a copy of the License at\n\n");
@@ -47,22 +34,24 @@ void licenses()
 void help()
 {
     printf("Usage (arguments): \n");
+    printf("   -b, --backup      backup mode\n");
+    printf("   -f, --flash       flash mode\n");
     printf("   -p, --partition   name of the partition to be backed up\n");
     printf("   -l, --logical     know that the partition that will be backed up is logical\n");
-    printf("   -o, --out         the output name of the backed-up partition (default: partition name)\n");
-    printf("   -d, --outdir      directory where the backup partition will be saved (default: /storage/emulated/0)\n");
+    printf("   -o, --out         (only backups) the output name of the backed-up partition (default: partition name)\n");
+    printf("   -d, --outdir      (only backups) directory where the backup partition will be saved (default: /storage/emulated/0)\n");
     printf("   -c, --context     it is meant to specify a custom /dev context. Only classic partitions (default: /dev/block/by-name)\n");
     printf("   -D, --list        list partitions\n");
     printf("   -v, --version     see version\n");
     printf("   -h, --help        see help message\n");
     printf("   -L, --license     see license\n\n");
     printf("Example 1:\n");
-    printf("   -p boot_a -o boot_slot_a_image -d /sdcard/backup -c /dev/block/platform/bootdevice/by-name\n\n");
+    printf("   -b --partition boot_a -o boot_slot_a_image -d /sdcard/backup -c /dev/block/platform/bootdevice/by-name\n\n");
     printf("Example 2:\n");
+    printf("   --flash /sdcard/twrp/boot.img -p boot_a -c /dev/block/platform/bootdevice/by-name\n\n");
+    printf("Example 3:\n");
     printf("   -c /dev/block/platform/bootdevice/by-name --list\n\n");
     printf("Report bugs to <xda-@YZBruh>\n");
 }
 
 /* end of code */
-
-#endif
