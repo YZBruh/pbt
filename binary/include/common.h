2d23f4538737d1867ae5f368c30feb3f1ebb6bfa
YZBruh <yagizzengin73@gmail.com>
2024-03-06 20:26:56 +0300

    pmt: initial 1.5.0 update

--- binary/include/common.h
+++ binary/include/common.h
@@ -1,22 +1,9 @@
-#ifndef _COMMON_H_
 #define _COMMON_H_
 
 /* By YZBruh */
 
 /*
- *         _      _   
- *        | |    | |  
- *  _ __  | |__  | |_ 
- * | '_ \ | '_ \ | __|
- * | |_) || |_) || |_ 
- * | .__/ |_.__/  \__|
- * | |                
- * |_|                
- *
- */
-
-/*
- * Copyright 2024 YZBruh - Partition Packupper
+ * Copyright 2024 YZBruh - Partition Manager
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,43 +18,35 @@
  * limitations under the License.
  */
 
-/* funcs */
-void error(const char *err_msg);
-void check_psf();
-void check_root();
-void listpart();
-void backup(char *target_pt, char *pst);
-void flash(char *target_flash_part, char *target_flash_fpart);
-
-/* macros */
+/* color definations */
 #define ANSI_RED      "\033[31m"
 #define ANSI_YELLOW   "\033[33m"
 #define ANSI_GREEN    "\033[32m"
 #define ANSI_RESET    "\033[0m"
 
+/* info */
 #define PACK_VER      "1.5.0"
 #define PACK_VER_CODE "150"
-#define PACK_NAME     "Partition Backupper"
+#define PACK_NAME     "Partition Manager"
 #define PACK_LANG     "en"
 
-/* variables */
-extern char *argx_target_p;
+/* variable definations */
 extern char *out;
 extern char *outdir;
-extern char *my_out;
 extern char *cust_cxt;
-extern char target_flash_f[300];
-extern char cust_cxt_path[150];
-extern char cust_cxt_ckpath[150];
-extern char cust_cxt_cklpath[150];
-extern char path[300];
-extern char cmd[256];
+extern char *target_partition;
+extern char *target_flash_file;
 extern bool use_logical;
 extern bool use_cust_cxt;
-extern bool pbt_ab;
-extern bool pbt_logical;
-extern bool pbt_flash_mode;
+extern bool pmt_ab;
+extern bool pmt_logical;
+extern bool pmt_flash;
+extern bool pmt_backup;
 
-/* end of code */
-
-#endif
+/* function definations */
+void listpart();
+void error(const char *err_msg);
+void check_psf();
+void check_root();
+void backup(char *target_backup_partition, char *backup_partition_style);
+void flash(char *target_flash_partition, char *target_file, char *flash_partition_style);
