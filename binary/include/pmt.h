711319cce0979d9dc331ceab7936dff04570175f
YZBruh <yagizzengin73@gmail.com>
2024-04-10 00:51:39 +0300

    Update pmt.h

--- binary/include/pmt.h
+++ binary/include/pmt.h
@@ -28,7 +28,7 @@ extern "C" {
 #endif
 
 /* compiler architecture if arm is not 32-bit or 64-bit, the compilation is stopped */
-#if ! defined __aarch64__ || __aarch32__ || __armv8__ || __armv7l__ || armv7l
+#if ! defined __aarch64__ || __aarch32__ || __armv8__ || __armv7l__
     #error "only 32-bit or 64-bit arm compilers can be used"
 #endif
 
