4464c203fdedb9ed5fe6a5508b4ddacc454f3207
YZBruh <yagizzengin73@gmail.com>
2024-05-05 21:05:21 +0300

    pmt: initial 1.9.0 update

---
+++ src/versioner.c
@@ -0,0 +1,46 @@
+/* By YZBruh */
+
+/**
+ * Copyright 2024 Partition Manager
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "include/pmt.h"
+
+void version()
+{
+    printf("Version: %s (code %s)\n", PMT_VERSION, PMT_VERSION_CODE);
+    #ifdef __clang__
+        printf("Compiler: clang %s", __clang_version__);
+    #endif
+    #ifdef __GNUC__
+        printf("(GNUC %d.%d.%d)\n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+    #else
+        printf("\n");
+    #endif
+    printf("See licenses with -L argument.\n");
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/* end of code */
